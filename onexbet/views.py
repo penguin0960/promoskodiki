40b06074d0f05623b9ce076301304a7001bf44e9
Roman Posevin <posevin.2011@mail.ru>
2025-06-04 23:04:35 +0300

    ready for a lot of codes

--- onexbet/views.py
+++ onexbet/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'onexbet/main.html')
+    return render(
+        request,
+        'onexbet/main.html',
+        {'promocodes': [{'code': 'CODE1'}, {'code': 'CODE2'}]}
+    )
