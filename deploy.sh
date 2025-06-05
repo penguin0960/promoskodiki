ef84ff5fafd92e85220d966cbd1c16b201b754e0
Roman Posevin <posevin.2011@mail.ru>
2025-06-05 22:11:07 +0300

    deploy settings

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git reset --hard
 git pull
 ./venv/bin/pip install -r ./requirements.txt
 ./venv/bin/python3 ./manage.py migrate
-./venv/bin/python3 ./manage.py collectstatic
+./venv/bin/python3 ./manage.py collectstatic --noinput
 fuser -k 8000/tcp
 ./venv/bin/gunicorn --workers 3 --bind 127.0.0.1:8000 promokodiki.wsgi:application &
